4fb955b9a56e5bbcbaf36a2d4c9675d7358ff988
Lewis Heath <email@lewisheath.co.uk>
2021-10-28 00:02:12 +0100

    first commit

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div className="nav">
+            <p className='LaBelle'>&lt;html&gt;</p>
+            <div className='listWrapper'>
+                <ul className='CascadiaCode'>
+                    <li>hello()</li>
+                    <li>my()</li>
+                    <li>name()</li>
+                    <li>is()</li>
+                    <li>lewis()</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
